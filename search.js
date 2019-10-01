27ac39193efba1fcfdfd7c1175bea37951f270cb
profgrammer <javarep4199@gmail.com>
2019-10-01 21:13:55 +0530

    add port var to listen

--- search.js
+++ search.js
@@ -59,6 +59,6 @@ app.get('/search', (req, res) => {
     })
 })
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'));
 
 
